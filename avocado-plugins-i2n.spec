35f1fde4d4a78ea7db09ec69bb371757e902b7bd
Plamen Dimitrov <pdimitrov@pevogam.com>
2019-12-18 12:58:16 +0200

    Install the sample test suite by default
    This should be available for as a starting point to each test
    runner or developer installing the plugin.

--- avocado-plugins-i2n.spec
+++ avocado-plugins-i2n.spec
@@ -86,6 +86,7 @@ graph structure.
 %doc README.md LICENSE
 %{python3_sitelib}/avocado_i2n*
 %{python3_sitelib}/avocado_framework_plugins_i2n*
+%{_datadir}/avocado-plugins-i2n/tp_folder/*
 
 %changelog
 * Fri Jan 24 2020 Plamen Dimitrov <plamen.dimitrov@intra2net.com> - 75.0-0
--- setup.py
+++ setup.py
@@ -1,11 +1,33 @@
+# Copyright 2013-2020 Intranet AG and contributors
+#
+# Significant portion of this code was taken from the Avocado VT project with
+# Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
+#
+# avocado-i2n is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# avocado-i2n is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with avocado-i2n.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import sys
+import glob
 
 from setuptools import setup
 
 VERSION = open('VERSION', 'r').read().strip()
 
-VIRTUAL_ENV = hasattr(sys, 'real_prefix')
+
+def __is_virtual_env():
+    return (hasattr(sys, 'real_prefix') or (hasattr(sys, 'base_prefix') and
+                                            sys.base_prefix != sys.prefix))
 
 
 def get_dir(system_path=None, virtual_path=None):
@@ -17,7 +39,7 @@ def get_dir(system_path=None, virtual_path=None):
     """
     if virtual_path is None:
         virtual_path = system_path
-    if VIRTUAL_ENV:
+    if __is_virtual_env():
         if virtual_path is None:
             virtual_path = []
         return os.path.join(*virtual_path)
@@ -25,6 +47,31 @@ def get_dir(system_path=None, virtual_path=None):
         if system_path is None:
             system_path = []
         return os.path.join(*(['/'] + system_path))
+
+
+def get_data_files():
+    def add_files(level=[]):
+        installed_location = ['usr', 'share', 'avocado-plugins-i2n']
+        installed_location += level
+        level_str = '/'.join(level)
+        if level_str:
+            level_str += '/'
+        file_glob = '%s*' % level_str
+        files_found = [path for path in glob.glob(file_glob) if
+                       os.path.isfile(path)]
+        return [((get_dir(installed_location, level)), files_found)]
+
+    data_files = []
+    data_files_dirs = ['tp_folder']
+
+    for data_file_dir in data_files_dirs:
+        for root, dirs, files in os.walk(data_file_dir):
+            for subdir in dirs:
+                rt = root.split('/')
+                rt.append(subdir)
+                data_files += add_files(rt)
+
+    return data_files
 
 
 setup(name='avocado-framework-plugins-i2n',
@@ -36,6 +83,7 @@ setup(name='avocado-framework-plugins-i2n',
       packages=['avocado_i2n', 'avocado_i2n.plugins', 'avocado_i2n.cartgraph', 'avocado_i2n.vmnet'],
       package_data={'avocado_i2n.vmnet': ['templates/*.template']},
       install_requires=['avocado-framework-plugins-vt', 'aexpect'],
+      data_files=get_data_files(),
       include_package_data=True,
       entry_points={
           'avocado.plugins.settings': [
