02181ce36899122fe33a16b8f5e64e8c461d758f
Plamen Dimitrov <pdimitrov@pevogam.com>
2020-04-20 23:08:48 +0300

    Cover the command parser with bare-bone minimal unit tests

--- selftests/test_cmd_parser.py
+++ selftests/test_cmd_parser.py
@@ -10,16 +10,41 @@ class CmdParserTest(unittest.TestCase):
 
     def setUp(self):
         self.config = {}
-        self.config["params"] = []
+        self.config["params"] = ["aaa=bbb"]
 
     def tearDown(self):
         pass
 
-    def test_parse_cmd(self):
-        self.config["params"] += ["aaa=bbb", "ccc"]
+    def test_param_dict(self):
+        self.config["params"] += ["ccc"]
         cmd.params_from_cmd(self.config)
         self.assertEqual(self.config["param_str"], "aaa = bbb\n")
 
+    def test_selected_vms(self):
+        cmd.params_from_cmd(self.config)
+        self.assertEqual(self.config["selected_vms"], self.config["available_vms"])
+
+        self.config["params"] += ["vms=vm1"]
+        cmd.params_from_cmd(self.config)
+        self.assertEqual(self.config["selected_vms"], ["vm1"])
+
+    def test_selected_vms_invalid(self):
+        self.config["params"] += ["vms=vmX"]
+        with self.assertRaises(ValueError):
+            cmd.params_from_cmd(self.config)
+
+    def test_selected_restr(self):
+        cmd.params_from_cmd(self.config)
+        self.assertIn("only normal\n", self.config["tests_str"])
+
+        self.config["params"] += ["only=minimal"]
+        cmd.params_from_cmd(self.config)
+        self.assertIn("only minimal\n", self.config["tests_str"])
+
+    def test_selected_restr_invalid(self):
+        self.config["params"] += ["default_only=nonminimal"]
+        with self.assertRaises(ValueError):
+            cmd.params_from_cmd(self.config)
 
 if __name__ == '__main__':
     unittest.main()
