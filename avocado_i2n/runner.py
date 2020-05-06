da9d213f046edfca4830917f41bf51c57cf02b0f
Plamen Dimitrov <pdimitrov@pevogam.com>
2020-05-06 13:27:53 +0300

    Fix ephemeral and general object state tracking for elaborate setup cases
    Using the state that was last retrieved instead of the state that
    was last stored is more reasonable way to track at what state a
    given test object is at. We cannot track unsaved changes from a
    test anyway so let's track the cleaner restore points.

--- avocado_i2n/runner.py
+++ avocado_i2n/runner.py
@@ -324,7 +324,7 @@ class CartesianRunner(TestRunner):
                 object_name = test_object.name
                 object_params = test_node.params.object_params(object_name)
                 # if previous state is not known keep behavior assuming that the user knows what they are doing
-                if object_params.get("set_state") != test_object.current_state != "unknown":
+                if object_params.get("get_state") != test_object.current_state != "unknown":
                     logging.debug("Re-running ephemeral setup %s since %s state was switched to %s",
                                   test_node.params["shortname"], test_object.name, test_object.current_state)
                     test_node.should_run = True
@@ -357,8 +357,8 @@ class CartesianRunner(TestRunner):
             for test_object in test_node.objects:
                 object_name = test_object.name
                 object_params = test_node.params.object_params(object_name)
-                if object_params.get("set_state") is not None and object_params.get("set_state") != "":
-                    test_object.current_state = object_params.get("set_state")
+                if object_params.get("get_state") is not None and object_params.get("get_state") != "":
+                    test_object.current_state = object_params.get("get_state")
             test_node.should_run = False
         else:
             logging.debug("Skipping test %s", test_node.params["shortname"])
