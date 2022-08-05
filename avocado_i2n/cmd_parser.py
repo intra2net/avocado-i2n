17cd5d6dc4606e41f2b960ffad46496fff2f7b53
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2022-08-05 12:40:39 +0300

    Set a default image state backend for the ramfile vm state backend
    We can choose at least between a local and a remote (pool) backend
    so specify the default choice in code for now so that it could be
    overriden later.

--- avocado_i2n/cmd_parser.py
+++ avocado_i2n/cmd_parser.py
@@ -143,6 +143,7 @@ def params_from_cmd(config):
                    "lxc": lxc.LXCBackend, "btrfs": btrfs.BtrfsBackend,
                    "qcow2vt": qcow2.QCOW2VTBackend, "ramfile": ramfile.RamfileBackend,
                    "vmnet": vmnet.VMNetBackend}
+    ramfile.RamfileBackend.image_state_backend = pool.QCOW2PoolBackend
 
     # attach environment processing hooks
     env_process_hooks()
