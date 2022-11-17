f8a25b85da33107ec6c3611cfda2f6be4072d060
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2022-11-17 12:10:38 +0200

    Move previously shared root manipulation methods to the qcow2 backends
    This means our inherited state backend class is now purely abstract
    and no state backend inheriting from it will assume QCOW2 compatibility
    and behavior. The LVM state backend that was previously limited to
    using QCOW2 for it root set operation will now simply reimplement this
    part of the code keeping the old todo only locally.
    
    Among other things, this simplifies the state backend structure further
    and replaces implicit inheritance with clearer separation of concerns.

--- avocado_i2n/states/setup.py
+++ avocado_i2n/states/setup.py
@@ -32,7 +32,6 @@ import logging as log
 logging = log.getLogger('avocado.test.' + __name__)
 
 from avocado.core import exceptions
-from virttest import env_process
 
 
 #: list of all available state backends and operations
@@ -121,24 +120,7 @@ class StateBackend():
         :returns: whether the object (root state) is exists
         :rtype: bool
         """
-        vm_name = params["vms"]
-        image_name = params["image_name"]
-        logging.debug("Checking whether %s's %s exists (root state requested)",
-                      vm_name, image_name)
-        if not os.path.isabs(image_name):
-            image_name = os.path.join(params["images_base_dir"], image_name)
-        image_format = params.get("image_format", "qcow2")
-        logging.debug("Checking for %s image %s", image_format, image_name)
-        image_format = "" if image_format in ["raw", ""] else "." + image_format
-        if object is not None and object.is_alive():
-            logging.info("The required virtual machine %s is alive and it shouldn't be", vm_name)
-            return False
-        if os.path.exists(image_name + image_format):
-            logging.info("The required virtual machine %s's %s exists", vm_name, image_name)
-            return True
-        else:
-            logging.info("The required virtual machine %s's %s doesn't exist", vm_name, image_name)
-            return False
+        raise NotImplementedError("Cannot use abstract state backend")
 
     @classmethod
     def get_root(cls, params, object=None):
@@ -162,19 +144,7 @@ class StateBackend():
         :param object: object whose states are manipulated
         :type object: :py:class:`virttest.qemu_vm.VM` or None
         """
-        vm_name = params["vms"]
-        if object is not None and object.is_alive():
-            object.destroy(gracefully=params.get_boolean("soft_boot", True))
-        image_name = params["image_name"]
-        if not os.path.isabs(image_name):
-            image_name = os.path.join(params["images_base_dir"], image_name)
-        image_format = params.get("image_format")
-        image_format = "" if image_format in ["raw", ""] else "." + image_format
-        if not os.path.exists(image_name + image_format):
-            os.makedirs(os.path.dirname(image_name), exist_ok=True)
-            logging.info("Creating image %s for %s", image_name, vm_name)
-            params.update({"create_image": "yes", "force_create_image": "yes"})
-            env_process.preprocess_image(None, params, image_name)
+        raise NotImplementedError("Cannot use abstract state backend")
 
     @classmethod
     def unset_root(cls, params, object=None):
@@ -186,19 +156,7 @@ class StateBackend():
         :param object: object whose states are manipulated
         :type object: :py:class:`virttest.qemu_vm.VM` or None
         """
-        vm_name = params["vms"]
-        if object is not None and object.is_alive():
-            object.destroy(gracefully=params.get_boolean("soft_boot", True))
-        image_name = params["image_name"]
-        if not os.path.isabs(image_name):
-            image_name = os.path.join(params["images_base_dir"], image_name)
-        logging.info("Removing image %s for %s", image_name, vm_name)
-        params.update({"remove_image": "yes"})
-        env_process.postprocess_image(None, params, image_name)
-        try:
-            os.rmdir(os.path.dirname(image_name))
-        except OSError as error:
-            logging.debug("Image directory not yet empty: %s", error)
+        raise NotImplementedError("Cannot use abstract state backend")
 
 
 #: available state backend implementations
