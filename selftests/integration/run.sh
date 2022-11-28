a2eebc374bfbf7e46277f2d240ad40d45ad16833
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2022-11-28 13:20:16 +0200

    Add a full integration run to the integration tests
    This is now possible since we are not restricted by a Travis CI
    docker container and can do full integration testing on our own
    platform and by extension environment.

--- selftests/integration/run.sh
+++ selftests/integration/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu
 
+# minimal effect runs
 coverage run --append --source=avocado_i2n $(which avocado) manu setup=noop
 coverage run --append --source=avocado_i2n $(which avocado) manu setup=list
 
@@ -8,3 +9,5 @@ coverage run --append --source=avocado_i2n $(which avocado) manu setup=list
 #coverage run --append --source=avocado_i2n $(which avocado) list --auto "only=tutorial1"
 #coverage run --append --source=avocado_i2n $(which avocado) run --auto "only=tutorial1 dry_run=yes"
 
+# full integration run
+coverage run --append --source=avocado_i2n $(which avocado) manu setup=run slots=c101,c102,c103,c104,c105 only=leaves
