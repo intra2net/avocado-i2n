88b4c5cb3cff53a0f0fdc43f92752372148642af
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2022-12-03 06:39:55 +0200

    Test equalized (synced) state setup across all swarm environments
    Once a test completes we have to make sure all previously achieved,
    shared, and reused setup across the worker swarm is available in a
    way that is independent of which test set we would run next. In other
    words, no worker should recreate the setup and thus mutate it if it
    happens to traverse a path others have already traversed from a previous
    run.

--- selftests/integration/run.sh
+++ selftests/integration/run.sh
@@ -16,13 +16,20 @@ test_options="cartgraph_verbose_level=0"
 coverage run --append --source=avocado_i2n $(which avocado) manu setup=run slots=$test_slots only=$test_sets $test_options
 
 # custom checks
-# check graph verbosity after the complete test suite run
+echo
+echo "Check graph verbosity after the complete test suite run"
 test -f /mnt/local/results/latest/cg_*.svg
 test -d /mnt/local/results/latest/graph_parse
 find /mnt/local/results/latest/graph_parse/cg_*.svg > /dev/null
 test -d /mnt/local/results/latest/graph_traverse
 find /mnt/local/results/latest/graph_traverse/cg_*.svg > /dev/null
-# TODO: check if all containers have the same states with identical hash sums
-# TODO: further custom hooks
+
+echo
+echo "Check if all containers have identical and synced states after the run"
+ims="mnt/local/images"
+containers="$(printf $test_slots | sed "s/,/ /g")"
+for cid in $containers; do
+    diff -r /$ims/c101/rootfs/$ims /$ims/$cid/rootfs/$ims -x el8-64* -x win10-64* -x vm3
+done
 
 echo "Integration tests passed successfully"
