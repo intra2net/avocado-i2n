b21232526ccbb273924dcb03fdb7e8871db453d3
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2024-10-02 21:05:33 +0300

    move remote door usage from Pyro4 to Pyro5
    TODO: Should we also rely on Pyro5 compatible aexpect
    remote door first?

--- tp_folder/tests/tutorial_step_3.py
+++ tp_folder/tests/tutorial_step_3.py
@@ -122,12 +122,12 @@ def run(test, params, env):
         door.REMOTE_PYTHON_BINARY = "python3.6"
 
         # The most advanced remote methods require serialization backend.
-        serialization_cmd = door.REMOTE_PYTHON_BINARY + " -c 'import Pyro4'"
+        serialization_cmd = door.REMOTE_PYTHON_BINARY + " -c 'import Pyro5'"
         guest_serialization = server_vm.session.cmd_status(serialization_cmd) == 0
         if not guest_serialization:
             logging.warning("The remote door object backend not found on guest")
         try:
-            import Pyro4
+            import Pyro5
         except ImportError:
             logging.warning("The remote door object backend not found on host")
             host_serialization = False
